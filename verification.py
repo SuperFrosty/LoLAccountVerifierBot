da43c2a932f2b12b6b18ba18ddb543cbbc1ed7db
Alex Palmer <104133.palmer@gmail.com>
2017-08-04 00:33:29 +1000

    Bug fixes, clarity, improvements, and API adherance

--- verification.py
+++ verification.py
@@ -1,4 +1,6 @@
 import cassiopeia as cass
+from cassiopeia.configuration import settings
+from cassiopeia.core import RunePages
 import random
 import string
 import discord
@@ -12,6 +14,11 @@ class Verification:
 
     @commands.command()
     async def verify(self, ctx, sum_name: str, region: str):
+        
+        if region == 'KR':
+            await ctx.send("", embed=discord.Embed(colour=0xCA0147, title="Error!", description="Riot recently disallowed rune page verification of korean accounts, sorry!"))
+            return
+
         random_string = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
         try:
             await ctx.author.send("", embed=discord.Embed(colour=0x1AFFA7, title="Instructions:", description="Please rename your first rune page to `{}`, wait a while, then reply with 'done'.".format(random_string)))
@@ -29,6 +36,7 @@ class Verification:
         except asyncio.TimeoutError:
             await ctx.author.send("", embed=discord.Embed(colour=0xCA0147, title="Error!", description="You didn't respond in time."))
         else:
+            await ctx.author.trigger_typing()
             summoner = cass.Summoner(name=sum_name, region=region)
             if summoner.exists:
                 rank_solo, rank_flex, rank_tt = '', '', ''
@@ -45,7 +53,7 @@ class Verification:
                             rank_tt = league.tier.value
                     ranks = [rank_solo, rank_flex, rank_tt]
                     numeric_ranks = []
-                    for rank in rank_solo:
+                    for rank in ranks:
                         if rank == 'CHALLENGER':
                             numeric_ranks.append(7)
                         elif rank == 'MASTER':
@@ -65,37 +73,39 @@ class Verification:
                     highest_rank = max(numeric_ranks)
                     if highest_rank == 7:
                         role = discord.utils.find(lambda m: m.name == 'Challenger', ctx.guild.roles)
-                        await self.bot.add_roles(ctx.author, role)
+                        await ctx.author.add_roles(role, reason="Verified account.")
                         role_given = 'Challenger'
                     if highest_rank == 6:
                         role = discord.utils.find(lambda m: m.name == 'Master', ctx.guild.roles)
-                        await self.bot.add_roles(ctx.author, role)
+                        await ctx.author.add_roles(role, reason="Verified account.")
                         role_given = 'Master'
                     if highest_rank == 5:
                         role = discord.utils.find(lambda m: m.name == 'Diamond', ctx.guild.roles)
-                        await self.bot.add_roles(ctx.author, role)
+                        await ctx.author.add_roles(role, reason="Verified account.")
                         role_given = 'Diamond'
                     if highest_rank == 4:
                         role = discord.utils.find(lambda m: m.name == 'Platinum', ctx.guild.roles)
-                        await self.bot.add_roles(ctx.author, role)
+                        await ctx.author.add_roles(role, reason="Verified account.")
                         role_given = 'Platinum'
                     if highest_rank == 3:
                         role = discord.utils.find(lambda m: m.name == 'Gold', ctx.guild.roles)
-                        await self.bot.add_roles(ctx.author, role)
+                        await ctx.author.add_roles(role, reason="Verified account.")
                         role_given = 'Gold'
                     if highest_rank == 2:
                         role = discord.utils.find(lambda m: m.name == 'Silver', ctx.guild.roles)
-                        await self.bot.add_roles(ctx.author, role)
+                        await ctx.author.add_roles(role, reason="Verified account.")
                         role_given = 'Silver'
                     if highest_rank == 1:
                         role = discord.utils.find(lambda m: m.name == 'Bronze', ctx.guild.roles)
-                        await self.bot.add_roles(ctx.author, role)
+                        await ctx.author.add_roles(role, reason="Verified account.")
                         role_given = 'Bronze'
-                    await ctx.author.send("", embed=discord.Embed(colour=0x1AFFA7, title="Success!", description="You've been given the {} role.".format(role_given)))
+                    await ctx.author.send("", embed=discord.Embed(colour=0x1AFFA7, title="Success!", description="You've been given the `{}` role.".format(role_given)))
+                    settings.pipeline._cache._cache._data[RunePages].clear()
                 else:
                     await ctx.author.send("", embed=discord.Embed(colour=0xCA0147, title="Error!", description="Could not verify first rune page. Please wait a bit for the name change to save."))
+                    settings.pipeline._cache._cache._data[RunePages].clear()
             else:
-                await ctx.send("", embed=discord.Embed(colour=0xCA0147, title="Error!", description="Couldn't find summoner '{0}' on the {1} region".format(summoner.name, region)))
+                await ctx.author.send("", embed=discord.Embed(colour=0xCA0147, title="Error!", description="Couldn't find summoner '{0}' on the {1} region".format(summoner.name, region)))
                 return
 
 def setup(bot):
